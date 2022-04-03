301745cf47bb07532cb68232a109e2aeada6a04a
FernandoQuicenoG <fqgomez@misena.edu.co>
2022-04-03 18:43:06 -0500

    primer commit

---
+++ src/main/java/com/example/ReactivaBibliotecaSpringBootMongoDB/ReactivaBibliotecaSpringBootMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.ReactivaBibliotecaSpringBootMongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactivaBibliotecaSpringBootMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactivaBibliotecaSpringBootMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ReactivaBibliotecaSpringBootMongoDB/ReactivaBibliotecaSpringBootMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ReactivaBibliotecaSpringBootMongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactivaBibliotecaSpringBootMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
